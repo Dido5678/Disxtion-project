4a5082b5d671176da0419dd9689b2eec45b17775
Aida Mkrtchyan <aida.mkrtchyan0606@gmail.com>
2024-11-18 19:33:35 +0400

    add cover foto

--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -12,16 +12,16 @@ const Header: React.FC = () => {
           </div>
           <ul className={styles.header_items}>
               <li className={styles.header_box}>
-                <Link to="/about">About</Link>
+                <Link to="/about" className={styles.header_boxLink}>About</Link>
               </li>
               <li className={styles.header_box}>
-                <Link to="/experience">Experience</Link>
+                <Link to="/experience" className={styles.header_boxLink}>Experience</Link>
               </li>
               <li className={styles.header_box}>
-                <Link to="/work">Work</Link>
+                <Link to="/work" className={styles.header_boxLink}>Work</Link>
               </li>
               <li className={styles.header_box}>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact" className={styles.header_boxLink}>Contact</Link>
               </li>
               <li className={styles.header_box}>
                   <Link to="/Aida%20Resume.pdf" className={styles.header_resume} target="_blank" rel="noopener noreferrer">Resume</Link>
